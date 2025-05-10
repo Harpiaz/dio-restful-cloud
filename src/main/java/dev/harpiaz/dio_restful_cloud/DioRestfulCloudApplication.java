f37a15fc8caa929c052aa6315e63fe8889a777db
Harpiaz <matffc@hotmail.com>
2025-05-10 20:52:31 -0300

    first commit

---
+++ src/main/java/dev/harpiaz/dio_restful_cloud/DioRestfulCloudApplication.java
@@ -0,0 +1,13 @@
+package dev.harpiaz.dio_restful_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioRestfulCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioRestfulCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/harpiaz/dio_restful_cloud/DioRestfulCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.harpiaz.dio_restful_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioRestfulCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
